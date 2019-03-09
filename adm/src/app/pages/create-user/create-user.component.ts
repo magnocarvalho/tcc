4b7e0154c3bbfd277c47d28fa1b9158702b9ec3a
Magno Carvalho <magnocarv@hotmail.com>
2019-03-09 19:58:53 -0300

    inicio do dashboard de ajustes de fixed

--- adm/src/app/pages/create-user/create-user.component.ts
+++ adm/src/app/pages/create-user/create-user.component.ts
@@ -2,6 +2,7 @@ import { Router } from "@angular/router";
 import { AuthService } from "./../../services/auth.service";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { Component, OnInit } from "@angular/core";
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: "app-create-user",
@@ -9,7 +10,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./create-user.component.css"]
 })
 export class CreateUserComponent implements OnInit {
-  constructor(public auth: AuthService, public route: Router) { }
+  constructor(public auth: AuthService, public route: Router, private snackBar: MatSnackBar) { }
 
   form = new FormGroup({
     email: new FormControl("", Validators.email),
@@ -20,22 +21,47 @@ export class CreateUserComponent implements OnInit {
 
   ngOnInit() { }
   criarLogin() {
-
+    if(!this.form.valid){
+      this.snackBar.open('Preencha todos os campos', "erro", {
+        duration: 5000,
+      });
+      return;
+    }
+    if (!this.verificaSenhas()) {
+      return;
+    }
+    var senhas = this.form.get("senha1").value;
+    var nome = this.form.get("nome").value;
+    var email = this.form.get("email").value;
     var obj = {
       email: this.form.get("email").value,
-      senha: this.form.get("senha").value
+      senha: senhas
     };
     this.auth.createUser(obj).then(
       () => {
-        console.log("deu bom!!!");
-        this.route.navigate(["dashboard"]);
+        this.snackBar.open('Verifique seu email!', 'Sucesso', {
+          duration: 5000,
+        });
+        // this.route.navigate(["dashboard"]);
       },
       err => {
-        console.log(err);
+        var erro = err;
+        this.snackBar.open(erro.message, "erro", {
+          duration: 5000,
+        });
       }
     );
   }
   verificaSenhas(): boolean {
-    return true;
+    var s1 = this.form.get("senha1").value;
+    var s2 = this.form.get("senha2").value;
+    if (s1 === s2) {
+      return true;
+    } else {
+      this.snackBar.open('Senhas não conferem!', 'Erro', {
+        duration: 5000,
+      });
+      return false;
+    }
   }
 }
--- adm/src/app/pages/dashboard/dashboard.component.ts
+++ adm/src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { COMMA, ENTER } from "@angular/cdk/keycodes";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +10,77 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  constructor(public api: ApiService) {}
+  form;
+
+  categorias = [];
+  tag: Tags[] = [];
+  fotos: any[] = [];
+  fotoThumb: any[] = [{}];
+  fotosBlocos: any[] = [{}];
+  fotoPerfil: any[] = [{}];
+  step = 0;
+  postagem = [];
+  textoP = "";
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  blog;
+  htmlContent;
+  configuracao = {
+    editable: true,
+    spellcheck: true,
+    height: "auto",
+    minHeight: "200",
+    width: "auto",
+    minWidth: "0",
+    translate: "yes",
+    enableToolbar: true,
+    showToolbar: true,
+    fontSize: "20px",
+    fontSizePopover: "16px",
+    placeholder: "Texto aqui..",
+    imageEndPoint: "",
+    toolbar: [
+      ["bold", "italic", "underline"],
+      ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull"],
+      ["link", "unlink"]
+    ]
+  };
+  editando = false;
+  editavel;
+  toRemove;
+
+  customCollapsedHeight: string = '';
+  customExpandedHeight: string = '';
+
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+
+  constructor(public api: ApiService, private router: Router, private zone: NgZone) {
+    this.form = new FormGroup({
+      // "empresa": new FormControl('', [Validators.required]),
+      titulo: new FormControl("", [Validators.required]),
+      subtitulo: new FormControl(""),
+      dataCriacao: new FormControl(moment().format("L"), [Validators.]),
+      dataPublicacao: new FormControl(moment().format("L"), [ValidateDate]),
+      publicado: new FormControl(""),
+      bgtitulo: new FormControl(""),
+      blocos: new FormControl(""),
+      autorSobre: new FormControl(""),
+      tags: new FormControl(""),
+      autor: new FormControl("", [Validators.required]),
+      thumbnail: new FormControl(""),
+      imgAutor: new FormControl(""),
+      url: new FormControl("", [Validators.required]),
+      resumo: new FormControl(""),
+      textoParagrafo: new FormControl(""),
+      textoSubtitulo: new FormControl(""),
+      tipoPostagem: new FormControl(""),
+      IdCategoria: new FormControl(),
+      isDestaque: new FormControl()
+    });
+
+  }
   salvar() {
     this.api.postagemAdd('testando');
   }

cf81cc284546d5d0a3974e39a986357e016bfeae
Magno Carvalho <magnocarv@hotmail.com>
2019-07-02 18:56:27 -0300

    inicio do backend estruturação backend

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,43 +1,47 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-import * as express from 'express';
-import * as path from 'path';
-import * as mongoose from 'mongoose';
-import * as bodyParser from 'body-parser';
-import * as routes from './routes/api';
-
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+// import routes from './routes/Rotas';
+// import PagamentoCtrl from './controllers/PagamentoCtrl';
 
 var app = express();
 
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: '2mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.DB_HOST,  { poolSize: 5 });
+mongoose.set('useFindAndModify', false); // biblioteca depreciada
+mongoose.connect('mongodb://localhost:27017/tcc', { useNewUrlParser: true });
+
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Method', 'PUT, POST, DELETE, GET');
     res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
     next();
 });
 
-app.use('/api', routes);
-app.use(express.static(path.join(__dirname, 'front')));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
 app.options('*', function (req, res, next) {
     if (req.method == 'OPTIONS')
+        // res.status(200);
         res.sendStatus(200);
+
 });
 
+// app.use('/', routes);
 
 app.use(function (req, res, next) {
     let err: any;
     err = new Error('Not found');
     err.status = 404;
     next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message || "Something went wrong. Please try again",
+        status: err.status || 500
+    });
 });
 
 if (app.get('env') === 'development') {
@@ -50,12 +54,5 @@ if (app.get('env') === 'development') {
     });
 }
 
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        message: err.message,
-        error: {}
-    });
-});
-var port = process.env.PORT || 1337;
+var port = 1337;
 app.listen(port);

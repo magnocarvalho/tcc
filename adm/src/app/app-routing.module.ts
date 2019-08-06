4b6445e06806c6202bd98145de90e8a60cf15808
Magno Carvalho <magnocarv@hotmail.com>
2019-08-05 22:16:51 -0300

    inicio do design de layout

--- adm/src/app/app-routing.module.ts
+++ adm/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MainComponent } from "./main/main.component";
+import { LogoutComponent } from "./logout/logout.component";
+import { LoginComponent } from "./login/login.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: MainComponent },
+  { path: "logout", component: LogoutComponent },
+  { path: "login", component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- adm/src/app/app.module.ts
+++ adm/src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MainComponent } from "./main/main.component";
+import { LogoutComponent } from "./logout/logout.component";
+import { LoginComponent } from "./login/login.component";
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MenuComponent } from './layout/menu/menu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainComponent, LogoutComponent, LoginComponent, HeaderComponent, FooterComponent, MenuComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

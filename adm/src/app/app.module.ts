5e48d0f946748e6e81602c7069aac157bf24b499
Magno Carvalho <magnocarv@hotmail.com>
2019-08-05 22:32:26 -0300

    primeiro modulo criado

--- adm/src/app/app.module.ts
+++ adm/src/app/app.module.ts
@@ -6,13 +6,28 @@ import { AppComponent } from "./app.component";
 import { MainComponent } from "./main/main.component";
 import { LogoutComponent } from "./logout/logout.component";
 import { LoginComponent } from "./login/login.component";
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { MenuComponent } from './layout/menu/menu.component';
+import { HeaderComponent } from "./layout/header/header.component";
+import { FooterComponent } from "./layout/footer/footer.component";
+import { MenuComponent } from "./layout/menu/menu.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material/material.module";
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, LogoutComponent, LoginComponent, HeaderComponent, FooterComponent, MenuComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    MainComponent,
+    LogoutComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ adm/src/app/material/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+
+@NgModule({
+  imports: [BrowserAnimationsModule, MatButtonModule, MatCheckboxModule]
+})
+export class MaterialModule {}
